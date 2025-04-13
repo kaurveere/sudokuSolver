3a36bc32f9d6063a16a5ce3d0e4c98860a6ffa2e
kaurveere <116031264+kaurveere@users.noreply.github.com>
2025-04-13 22:05:34 +0300

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,38 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include<QDebug>
+#include <string>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->tableWidget->setItem(3, 4, new QTableWidgetItem("9"));
+    ui->tableWidget->setItem(3, 3, new QTableWidgetItem("6"));
+    ui->tableWidget->setItem(4, 3, new QTableWidgetItem("4"));
+    ui->tableWidget->setItem(4, 4, new QTableWidgetItem("2"));
+    ui->tableWidget->setItem(4, 5, new QTableWidgetItem("0"));
+
+
+    for (int row = 0; row < 9; ++row) {
+        std::string ridad;
+        for (int col = 0; col < 9; ++col) {
+            QTableWidgetItem* item = ui->tableWidget->item(row, col);
+            QString value = item ? item->text() : "0";  // or "0" or "", depending on what you prefer
+            ridad += value.toStdString();
+        }
+        qInfo() << ridad;
+    }
+
+}
+
